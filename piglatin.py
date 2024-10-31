65306fdc1dc7232e71d453140e3de6b26bd31d9c
Hyrkas <Miika.Hyrkas@student.oulu.fi>
2024-10-31 17:19:30 +0200

    US3 - Test3

--- piglatin.py
+++ piglatin.py
@@ -7,7 +7,8 @@ class PigLatin:
         return self.phrase
 
     def translate(self) -> str:
-        vowels = ("a", "e", "i", "o", "u")
+        vowels = ("a", "e", "i", "o", "u", "ä", "ö", "å")
+        consonants = ("b", "c", "d", "f", "g", "h", "j", "k", "l", "m", "n", "p", "q", "r", "s", "t", "v", "w", "x", "z")
 
         if not self.get_phrase():
             return "nil"
@@ -16,5 +17,7 @@ class PigLatin:
             return self.get_phrase() + "nay"
         elif self.get_phrase().endswith(vowels):
             return self.get_phrase() + "yay"
+        elif self.get_phrase().endswith(consonants):
+            return self.get_phrase() + "ay"
 
         return self.get_phrase()
